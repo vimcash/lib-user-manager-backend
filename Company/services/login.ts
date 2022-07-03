fc0a8d29eeb1a99d1087aa1e4bd9219cfa996ebb
Raian M. Gil del Rosario <Raian.gdr@gmail.com>
2022-07-03 19:11:40 -0400

    fix: Simplificando el uso del login

--- Company/services/login.ts
+++ Company/services/login.ts
@@ -1,7 +1,7 @@
 import { getUser } from "../dao";
 import { comparePass } from "../middlewares/comparePass";
 
-export const login = async (username: string, password: string) => {
+export const login = async ({username, password}) => {
     const user = await getUser(username);
     return user ? comparePass(user, password) : "error"
 }
