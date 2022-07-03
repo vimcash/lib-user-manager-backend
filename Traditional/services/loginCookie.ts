81d994e848d07823502655e1bf8b07db96ed031f
eloyk <stifler9.0@hotmail.com>
2022-07-03 17:36:29 -0400

    fix: se reformatea login

--- Traditional/services/loginCookie.ts
+++ Traditional/services/loginCookie.ts
@@ -2,10 +2,11 @@ import config from "../config/config";
 import jwt from 'jsonwebtoken';
 import { getUser } from "../dao";
 import { comparePass } from "../middlewares/comparePass";
+import { TraditionalUserModel } from "vcl-model";
 
 export const loginCookie = async (username: string, password: string) => {
 
-    const user = await getUser(username);
+    const user = await TraditionalUserModel.findOne({username});
 
     // if (!user) {
     //     return "Error"
