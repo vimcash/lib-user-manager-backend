b1b15a1d8d3b1c19b6345a7742f97e3898055510
eloyk <stifler9.0@hotmail.com>
2022-07-09 12:44:48 -0400

    cambios

--- Traditional/services/loginCookie.ts
+++ Traditional/services/loginCookie.ts
@@ -2,19 +2,23 @@ import config from "../config/config";
 import jwt from 'jsonwebtoken';
 import { getUser } from "../dao";
 import { comparePass } from "../middlewares/comparePass";
-import { TraditionalUserModel } from "vcl-model";
 
 export const loginCookie = async (username: string, password: string) => {
 
-    const user = await TraditionalUserModel.findOne({username});
+  try {
+    const user = await getUser(username);
 
-    // if (!user) {
-    //     return "Error"
-    // }
+    if (!user) {
+        return "Error"
+    }
   
-    // if (!comparePass(user, password)) {
-    //     return "Error"
-    // }
+    if (!comparePass(user, password)) {
+        return "Error"
+    }
     
     return user
+
+  } catch (error) {
+    console.log(error);
+  }
 }
